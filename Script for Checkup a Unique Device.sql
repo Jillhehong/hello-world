652c00a009c95812880136b8bf70283ec9aee212
Jillhehong <jillhehong@gmail.com>
2016-05-04 09:39:04 -0400

    Added files via upload

---
+++ Script for Checkup a Unique Device.sql
@@ -0,0 +1,5 @@
+﻿SELECT "NO", "Date", "Device_SN", "Action", "By_Whom", "Status", "Owner", 
+       "Replace_Device", "Replace_Device_SN", "Notes"
+  FROM "Device_History" where "Device_SN"='1403287' order by "Date";
+
+  
